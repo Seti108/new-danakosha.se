5789bd07693a73911b25a1956a0a071a16050477
Seti108 <70008057+Seti108@users.noreply.github.com>
2021-03-24 16:24:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const footer = document.querySelector("footer");
 
 const main = document.querySelector("main");
 
+const mailPopup = document.querySelector(".mail-popup");
+
+const mail = document.querySelector(".fa-envelope");
+
 const width = window.innerWidth;
 
 // Menu toggle
@@ -75,3 +79,22 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach((img) => imgObserver.observe(img));
 
 console.log(window.innerHeight, window.innerWidth);
+
+// Show the mail adress on click
+mail.addEventListener("click", function () {
+  mailPopup.classList.toggle("hidden");
+  footer.style.filter = "blur(10px)";
+  main.style.filter = "blur(10px)";
+  main.style.userSelect = "none";
+  footer.style.userSelect = "none";
+
+  mailPopup.addEventListener("click", function (e) {
+    const btn = e.target.closest(".fa-times");
+    if (!btn) return;
+    mailPopup.classList.add("hidden");
+    footer.style.filter = "none";
+    main.style.filter = "none";
+    main.style.userSelect = "auto";
+    footer.style.userSelect = "auto";
+  });
+});
